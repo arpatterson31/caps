75322362905aaf7056f2e1d3bce63f7d29c1967c
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-03 14:26:03 -0700

    added order pickedup function to log driver picked up order after waiting one second

--- driver.js
+++ driver.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const events = require('./events.js');
+require('./vendor.js');
+
+events.on('pickup', orderPickup);
+
+function orderPickup(order) {
+  setTimeout(() => {
+    console.log(`DRIVER: picked up ${order.orderId}`);
+
+    events.emit('in-transit', order);
+  }, 1000)
+}
